dc7feff3cfbc01d8fe3a019dc749b36f5cf20268
emperorlu <emperorlu@hust.edu.cn>
2021-01-05 21:53:22 +0800

    [Debug] Add code for learned index

--- table/table.cc
+++ table/table.cc
@@ -77,7 +77,7 @@ Status Table::Open(const Options& options, RandomAccessFile* file,
     
     std::cout << __func__ << " stream: " <<  buf << std::endl;
     std::cout << __func__ << " stream: " <<  std::string(buf,buf+n) << std::endl;
-    std::stringstream stream(std::string(buf, n));
+    std::stringstream stream(std::string(buf,buf+n));
     // stream << buf;
     stream >> learnmod->max_lenth;
     std::cout << __func__ << " learnmod->max_lenth: " << learnmod->max_lenth << std::endl;
