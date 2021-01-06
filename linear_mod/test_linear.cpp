de5b049c154cd930283b182da912f140881cce60
emperorlu <emperorlu@hust.edu.cn>
2021-01-06 19:47:42 +0800

    [Debug] Add code for learned index

--- linear_mod/test_linear.cpp
+++ linear_mod/test_linear.cpp
@@ -9,10 +9,14 @@ int main(){
     
     // for (int i = 0; i < )
     mod::LRModel<> lr;
+    std::cout << " train before"<< std::endl;
     lr.train(x,y,10);
+    std::cout << " train end"<< std::endl;
     std::vector<u64> z;
     for(int i = 0; i < x.size(); i++){
+        std::cout << " predict before"<< std::endl;
         z[i] = lr.predict(x[i]);
+        std::cout << " predict end"<< std::endl;
         std::cout << z[i] << std::endl;
     }
     // predict(44);
