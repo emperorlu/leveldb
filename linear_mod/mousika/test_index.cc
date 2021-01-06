56126588c79be649492589b7fc11b83871f5ec32
emperorlu <emperorlu@hust.edu.cn>
2021-01-06 20:27:43 +0800

    [Debug] Add code for learned index

---
+++ linear_mod/mousika/test_index.cc
@@ -0,0 +1,69 @@
+// #include <gtest/gtest.h>
+
+#include "learned_index.h"
+#include <iostream>
+// #include "../src/datastream/rocksdb_stream.hpp"
+using namespace std;
+int main(){
+
+  RMIConfig rmi_config;
+  RMIConfig::StageConfig first, second;
+
+  // XD: which is first, and which is second means?
+  // does this means that there are only 2 stages for the model?
+  // pps: seems my guess is correct
+  first.model_type = RMIConfig::StageConfig::LinearRegression;
+  first.model_n = 1;
+
+  second.model_n = 12;
+  second.model_type = RMIConfig::StageConfig::LinearRegression;
+  rmi_config.stage_configs.push_back(first);
+  rmi_config.stage_configs.push_back(second);
+
+  LearnedRangeIndexSingleKey<uint64_t,float> table(rmi_config);
+  // RocksStream<uint64_t,uint64_t> db("./testdb");
+  // auto it = db.get_iter();
+
+  int count = 0;
+  // for(it->begin();it->valid();it->next()) {
+  //   auto key = it->key();
+  //   auto value = it->value();
+  //   LOG(4) << key;
+  //   table.insert(key,value);
+  //   count++;
+  // }
+  srand((unsigned)time(NULL)); 
+  vector<double> x;
+  vector<double> y;
+  // vector<double> z;
+  double key = 0;
+  double value = 0;
+  for (int i = 0; i < 1000; i++){
+    key += rand();
+    value += rand();
+    cout << key << ": " << value << endl;
+    table.insert(key,value);
+    // count++;
+    x.push_back(key);
+    y.push_back(value);
+  }
+  // ASSERT_EQ(count,12);
+  table.finish_insert();
+
+  LOG(4) << "finished insert";
+
+  for (int i = 0; i < 1000; i++){
+    key = x[i];
+    value = y[i];
+    cout << key << ": " << value << endl;
+    auto value_get = table.get(key);
+    cout << " value_get:" << value_get << endl;
+  }
+  // it->begin();
+  // for(it->begin();it->valid();it->next()) {
+  //   auto key = it->key();
+  //   auto value = table.get(key);
+  //   ASSERT_EQ(value,it->value());
+  // }
+
+} // end namespace test
