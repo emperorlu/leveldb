aa58109238ff2e1ab30d35025a09eadd8b827600
emperorlu <emperorlu@hust.edu.cn>
2021-01-06 10:06:12 +0800

    [Debug] Add code for learned index

--- mod/read_test.cc
+++ mod/read_test.cc
@@ -4,19 +4,25 @@
 using namespace std;
 
 int main(){
-    ifstream infile;
+    //ifstream infile;
     char buf[1024]; 
-    infile.open("/tmp/leveldbtest-0/dbbench/000005.ldb");
-    if(infile.is_open()){
-        while(infile.good() && !infile.eof()){
-        memset(buf,0,1024);
-        // infile.getline(buf,1204);
-        // message = buf;
-        // cout<<message<<endl;
-        infile.seekp(1808305,ios::beg);
-        infile >> buf; 
-        cout << buf << endl; 
-        }
-    }
+    fstream infile ("/tmp/leveldbtest-0/dbbench/000005.ldb", ios::in);
+    //infile.open("/tmp/leveldbtest-0/dbbench/000005.ldb");
+    // if(infile.is_open()){
+    //     while(infile.good() && !infile.eof()){
+    //     memset(buf,0,1024);
+    //     // infile.getline(buf,1204);
+    //     // message = buf;
+    //     // cout<<message<<endl;
+    //     infile.seekp(1808305,ios::beg);
+    //     infile >> buf; 
+    //     cout << buf << endl; 
+    //     }
+    // }
+    memset(buf,0,1024);
+        
+    infile.seekp(1808305,ios::beg);
+    infile >> buf; 
+    cout << buf << endl; 
     infile.close();
 }
