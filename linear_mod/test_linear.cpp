554c7ff64185438f4fedfe04ee5bcb2e1fc185e2
emperorlu <emperorlu@hust.edu.cn>
2021-01-06 19:38:52 +0800

    [Debug] Add code for learned index

--- linear_mod/test_linear.cpp
+++ linear_mod/test_linear.cpp
@@ -4,9 +4,9 @@
 using namespace mod;
 
 int main(){
-    std::vector<u64> x(1,2,44,53,421);
-    std::vector<u64> y(3,23,444,553,4432);
-
+    std::vector<u64> x = {1,2,44,53,421};
+    std::vector<u64> y = {3,23,444,553,4432};
+    
     // for (int i = 0; i < )
     mod::LRModel* lr = new LRModel;
     lr->train(x,y,10);
