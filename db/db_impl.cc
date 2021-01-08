487b6d390f7c4cb903da0eb2f8409594be344406
emperorlu <emperorlu@hust.edu.cn>
2021-01-08 20:01:40 +0800

    [Debug] Add code for learned index

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -1101,7 +1101,7 @@ Status DBImpl::Get(const ReadOptions& options, const Slice& key,
                    std::string* value) {
   Status s;
   std::cout << __func__ << " key1: " << key.ToStringHex() << " ;size:" << key.size()<< std::endl;
-  std::cout << __func__ << " key2: " << std::stod(key) << " ;size:" << key.size()<< std::endl;
+  // std::cout << __func__ << " key2: " << std::stod(key) << " ;size:" << key.size()<< std::endl;
   std::cout << __func__ << " key3: " << std::stod(key.data()) << " ;size:" << key.size()<< std::endl;
   MutexLock l(&mutex_);
   SequenceNumber snapshot;
