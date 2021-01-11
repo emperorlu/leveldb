9f8040aaa93e8d25bc345e1a44a69e50cab10f64
emperorlu <emperorlu@hust.edu.cn>
2021-01-11 20:06:18 +0800

    [Debug] Add code for learned index

--- table/table_builder.cc
+++ table/table_builder.cc
@@ -214,7 +214,7 @@ void TableBuilder::WriteLearnBlock(BlockHandle* handle) {
   
   Slice raw(param);
   // cPrintBuffer(LearnedMod->param, LearnedMod->lenth);
-  std::cout << __func__ << " param size:" << param.length() << " ;param: " << LearnedMod->param << std::endl;
+  std::cout << __func__ << " param size:" << param.length() << " ;param: " << param << std::endl;
 
   // TODO(postrelease): Support more compression options: zlib?
 
