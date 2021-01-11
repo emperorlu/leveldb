1795a960600b5254e1142aeba14134137570d4c5
emperorlu <emperorlu@hust.edu.cn>
2021-01-11 14:41:51 +0800

    [Debug] Add code for learned index

--- rmi/rmi.h
+++ rmi/rmi.h
@@ -402,10 +402,10 @@ class RMINew {
       second.push_back(stages.substr(pos, size));
       pos += size;
     }
-    std::cout << __func__ " :RMINew before memcpy" << std::endl;
+    std::cout << __func__ << " :RMINew before memcpy" << std::endl;
 
     memcpy(&key_n, stages.substr(pos, sizeof(key_n)).data() ,sizeof(key_n));
-    std::cout << __func__ " :RMINew memcpy" << std::endl;
+    std::cout << __func__ << " :RMINew memcpy" << std::endl;
     // std::cout << "key_n: " << key_n << std::endl;
     first_stage = new LRStage(first);
     second_stage = new LRStage(second);
