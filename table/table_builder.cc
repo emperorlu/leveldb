de208ab10cfd9a49b4644123037012220927758e
emperorlu <emperorlu@hust.edu.cn>
2021-01-11 16:48:39 +0800

    [Debug] Add code for learned index

--- table/table_builder.cc
+++ table/table_builder.cc
@@ -151,8 +151,8 @@ void TableBuilder::Add(const Slice& key, const Slice& value) {
   r->_bytes += value.size();
   Slice nkey (key.data(),8);
   uint64_t lekey = 0;
-  // memcpy(&lekey, nkey.data(), nkey.size());
-  lekey = strtoull (nkey.data(), NULL, 0);
+  memcpy(&lekey, nkey.data(), nkey.size());
+  // lekey = strtoull (nkey.data(), NULL, 0);
   std::cout << __func__ << " Add nkey: " << nkey.ToStringHex() << std::endl;
   std::cout << __func__ << " Add lekey: " << lekey << std::endl;
   LearnedMod->insert(lekey,r->_bytes);
@@ -298,8 +298,8 @@ Status TableBuilder::Finish() {
     for(auto& item: r->all_values){
       Slice nkey (item.first.data(),8);
       uint64_t lekey = 0;
-      // memcpy(&lekey, nkey.data(), nkey.size());
-      lekey = strtoull (nkey.data(), NULL, 0);
+      memcpy(&lekey, nkey.data(), nkey.size());
+      // lekey = strtoull (nkey.data(), NULL, 0);
       auto value_get = LearnedMod->get(lekey);
       std::cout << __func__ << " value_get: " << value_get << std::endl;
       int block_num = value_get / 4096;
