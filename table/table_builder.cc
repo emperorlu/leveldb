87818181cb544c1fe17c5aa86e7a4fcf8b6957cf
emperorlu <emperorlu@hust.edu.cn>
2021-01-11 16:24:52 +0800

    [Debug] Add code for learned index

--- table/table_builder.cc
+++ table/table_builder.cc
@@ -150,10 +150,10 @@ void TableBuilder::Add(const Slice& key, const Slice& value) {
   r->_bytes += key.size();
   r->_bytes += value.size();
   Slice nkey (key.data(),8);
-  std::cout << __func__ << " Add key: " << nkey.ToStringHex() << std::endl;
-  // std::cout << __func__ << ""
   uint64_t lekey = 0;
   memcpy(&lekey, nkey.data(), nkey.size());
+  std::cout << __func__ << " Add nkey: " << nkey.ToStringHex() << std::endl;
+  std::cout << __func__ << " Add lekey: " << lekey << std::endl;
   LearnedMod->insert(lekey,r->_bytes);
 
   // if (r->num_entries > 0) {
@@ -301,12 +301,14 @@ Status TableBuilder::Finish() {
       auto value_get = LearnedMod->get(lekey);
       std::cout << __func__ << " value_get: " << value_get << std::endl;
       int block_num = value_get / 4096;
+      std::cout << __func__ << " Add nkey: " << nkey.ToStringHex() << std::endl;
+      std::cout << __func__ << " Add lekey: " << lekey << std::endl;
+      std::cout << __func__ << " block_num: " << block_num << std::endl;
 
       if (r->num_entries > 0) {
         assert(r->options.comparator->Compare(item.first, Slice(r->last_key)) > 0);
       }
-      std::cout << __func__ << " key: " << nkey.ToStringHex() << std::endl;
-      std::cout << __func__ << " block_num: " << block_num << std::endl;
+
       if (r->pending_index_entry) {
         assert(r->data_block.empty());
         r->options.comparator->FindShortestSeparator(&r->last_key, item.first);
